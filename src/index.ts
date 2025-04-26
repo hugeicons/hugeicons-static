b09999ec2da6a81e1a2cf6be6e433bb76b33007b
Mohamed Tawfik <hello@halallab.co>
2025-04-27 00:05:13 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// This file will be auto-generated with all SVG exports
+export interface IconNode {
+  svg: string;
+  name: string;
+  tags: string[];
+}
+
+export const icons: { [key: string]: IconNode } = {};
+
+// Example of how icons will be exported:
+// export const homeIcon: IconNode = {
+//   svg: '<svg>...</svg>',
+//   name: 'home',
+//   tags: ['house', 'building', 'property']
+// }; 
